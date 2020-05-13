df1b9a61d6bd01e1d54c7599706afecdcddfe40f
Daniel Jordan <danieljordan86@gmail.com>
2020-05-13 14:00:55 -0600

    Implement store and local storage

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue';
-import App from './App.vue';
-import vuetify from './plugins/vuetify';
-import vuex from '@/store/store';
+import App from '@/App.vue';
+import vuetify from '@/plugins/vuetify';
+import store from '@/store/store';
 
 Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
-  vuex,
+  store,
   render: h => h(App),
 }).$mount('#app');
--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { saveStatePlugin } from '@/utils/utils';
 import sampleData from '@/data/sampleData';
 
 Vue.use(Vuex);
 
 const contacts = JSON.parse(localStorage.getItem('contacts')) || sampleData;
 
-export default new Vuex.store({
+const store = new Vuex.Store({
+  plugins: [saveStatePlugin],
   state: {
     contacts,
   },
   mutations: {},
   actions: {},
 });
+
+export default store;
